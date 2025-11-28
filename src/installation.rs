da12823b78a7b7d267b10b8b120d41b82c7d8e70
HorseNuggets <batchprogrammer314@gmail.com>
2025-11-27 20:57:20 -0800

    Use require-by-string in package links

--- src/installation.rs
+++ src/installation.rs
@@ -179,7 +179,7 @@ impl InstallationContext {
     /// Contents of a package-to-package link within the same index.
     fn link_sibling_same_index(&self, id: &PackageId) -> String {
         formatdoc! {r#"
-            return require(script.Parent.Parent["{full_name}"]["{short_name}"])
+            return require("../{full_name}/{short_name}")
             "#,
             full_name = package_id_file_name(id),
             short_name = id.name().name()
@@ -189,7 +189,7 @@ impl InstallationContext {
     /// Contents of a root-to-package link within the same index.
     fn link_root_same_index(&self, id: &PackageId) -> String {
         formatdoc! {r#"
-            return require(script.Parent._Index["{full_name}"]["{short_name}"])
+            return require("./_Index/{full_name}/{short_name}")
             "#,
             full_name = package_id_file_name(id),
             short_name = id.name().name()
